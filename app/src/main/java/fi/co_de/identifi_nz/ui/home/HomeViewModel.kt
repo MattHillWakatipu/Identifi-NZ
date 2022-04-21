5894506e06614c5c044af9a241f7421c530ba800
matt <matthillwakatipu@gmail.com>
2022-04-21 17:49:52 +1200

    Initial commit

---
+++ app/src/main/java/fi/co_de/identifi_nz/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package fi.co_de.identifi_nz.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
