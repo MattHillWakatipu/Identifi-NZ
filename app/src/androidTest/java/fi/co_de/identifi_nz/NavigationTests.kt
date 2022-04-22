5abadb9a82b5ffcd54df542b9bc2f09e28e46394
matt <matthillwakatipu@gmail.com>
2022-04-22 16:24:48 +1200

    Add navigation tests

--- app/src/androidTest/java/fi/co_de/identifi_nz/NavigationTests.kt
+++ app/src/androidTest/java/fi/co_de/identifi_nz/NavigationTests.kt
@@ -2,19 +2,23 @@ package fi.co_de.identifi_nz
 
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.launchActivity
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.*
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import fi.co_de.identifi_nz.ui.home.HomeFragment
 import org.junit.Test
 import org.junit.runner.RunWith
-
 import org.junit.Assert.*
-import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -24,51 +28,57 @@ import org.junit.Before
 @RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    // @Test
+    // fun nav_controller_test() {
+    //     // Init nav controller
+    //     val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+    //
+    //     // Launch starting fragment
+    //     val startScenario =
+    //         launchFragmentInContainer<HomeFragment>(themeResId = R.style.Theme_IdentifiNZ)
+    //
+    //     // Configure nav controller
+    //     startScenario.onFragment { fragment ->
+    //         navController.setGraph(R.navigation.mobile_navigation)
+    //         Navigation.setViewNavController(fragment.requireView(), navController)
+    //     }
+    //
+    //     // Check destination is correct
+    //     assertEquals(navController.currentDestination?.id, R.id.nav_profile)
+    // }
+
     @Test
-    @Before
-    fun setup() {
-        print("OK")
+    fun drawer_navigate_to_profile() {
+        check_navigation(R.id.nav_profile)
     }
 
     @Test
-    fun starts_on_home() {
-        // Init nav controller
-        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
-
-        // Launch starting fragment
-        val startScenario =
-            launchFragmentInContainer<HomeFragment>(themeResId = R.style.Theme_IdentifiNZ)
-
-        // Configure nav controller
-        startScenario.onFragment { fragment ->
-            navController.setGraph(R.navigation.mobile_navigation)
-            Navigation.setViewNavController(fragment.requireView(), navController)
-        }
-
-        // Click button
-        // onView(withId(R.id.app_bar_main)).perform(click())
-
-        // Check destination is correct
-        assertEquals(navController.currentDestination?.id, R.id.nav_home)
+    fun drawer_navigate_to_upload() {
+        check_navigation(R.id.nav_upload)
     }
 
     @Test
-    fun navigate_to_home() {
-        TODO()
+    fun drawer_navigate_to_verify() {
+        check_navigation(R.id.nav_verify)
     }
 
     @Test
-    fun navigate_to_profile() {
-        TODO()
+    fun drawer_navigate_to_home() {
+        check_navigation(R.id.nav_profile)
+        check_navigation(R.id.nav_home)
     }
 
-    @Test
-    fun navigate_to_upload() {
-        TODO()
-    }
+    private fun check_navigation(destinationId: Int) {
+        onView(withId(R.id.drawer_layout))
+            .perform(DrawerActions.open())
 
-    @Test
-    fun navigate_to_verify() {
-        TODO()
+        onView(withId(R.id.nav_view))
+            .perform(NavigationViewActions.navigateTo(destinationId))
+
+        onView(withId(destinationId))
+            .check(matches(isDisplayed()))
     }
 }
