6a0700bc806ae0d8248ea02b096b9406a64f54fb
matt <matthillwakatipu@gmail.com>
2022-05-09 15:15:24 +1200

    Create function to load identity fragments
    Currently hardcoded, should gather data from IPFS or similar source

--- app/src/main/java/fi/co_de/identifi_nz/data/Datasource.kt
+++ app/src/main/java/fi/co_de/identifi_nz/data/Datasource.kt
@@ -16,4 +16,40 @@ class Datasource {
             Transaction(R.string.transaction9, R.drawable.ic_activity_icon_request_outgoing),
         )
     }
+
+    fun loadIdentityFragments(): List<IdentityFragment> {
+        return listOf(
+            IdentityFragment(
+                R.drawable.ic_identity_fragment_icon_badge,
+                R.drawable.ic_identity_fragment_status_pending,
+                R.string.badge,
+                R.string.status_pending
+            ),
+            IdentityFragment(
+                R.drawable.ic_identity_fragment_icon_bank_account,
+                R.drawable.ic_identity_fragment_status_verified,
+                R.string.bank_account,
+                R.string.status_verified
+            ),
+            IdentityFragment(
+                R.drawable.ic_identity_fragment_icon_document,
+                R.drawable.ic_identity_fragment_status_verified,
+                R.string.document,
+                R.string.status_verified
+            ),
+            IdentityFragment(
+                R.drawable.ic_identity_fragment_icon_proof_of_address,
+                R.drawable.ic_identity_fragment_status_verified,
+                R.string.proof_of_address,
+                R.string.status_verified
+            ),
+            IdentityFragment(
+                R.drawable.ic_identity_fragment_icon_wallet_address,
+                R.drawable.ic_identity_fragment_status_verified,
+                R.string.wallet_address,
+                R.string.status_verified
+            ),
+        )
+    }
+
 }
