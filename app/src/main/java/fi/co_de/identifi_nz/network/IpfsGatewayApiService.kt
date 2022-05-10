a6fb6778e486bc796aba96e28974ba970b6d2f4f
matt <matthillwakatipu@gmail.com>
2022-05-10 13:48:40 +1200

    Create API for communicating with internet via Retrofit and Moshi

---
+++ app/src/main/java/fi/co_de/identifi_nz/network/IpfsGatewayApiService.kt
@@ -0,0 +1,43 @@
+package fi.co_de.identifi_nz.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val GATEWAY_URL = "https://ipfs.io/ipfs"
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
+/**
+ * Build the Moshi object with Kotlin adapter factory that Retrofit will be using.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * The Retrofit object with the Moshi converter.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the [getPhotos] method.
+ */
+interface IpfsGatewayApiService {
+    @GET("photos")
+    suspend fun getPhotos(): String
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service.
+ */
+object IpfsApi {
+    val retrofitService: IpfsGatewayApiService by lazy {
+        retrofit.create(IpfsGatewayApiService::class.java)
+    }
+}
