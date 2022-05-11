b6130259067d7406b6a993707b9d0e5094249187
matt <matthillwakatipu@gmail.com>
2022-05-11 15:37:19 +1200

    Modify Activity constructor to take an IpfsActivity

--- app/src/main/java/fi/co_de/identifi_nz/data/Activity.kt
+++ app/src/main/java/fi/co_de/identifi_nz/data/Activity.kt
@@ -1,9 +1,17 @@
 package fi.co_de.identifi_nz.data
 
 import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import fi.co_de.identifi_nz.R
+import fi.co_de.identifi_nz.network.json.IpfsActivity
 
-class Activity(
-    @StringRes val stringResourceId: Int,
-    @DrawableRes val imageResourceId: Int
-)
+class Activity(IpfsActivity: IpfsActivity) {
+    @DrawableRes
+    val imageResourceId = when (IpfsActivity.icon) {
+        "verified" -> R.drawable.ic_identity_fragment_status_verified
+        "outgoing" -> R.drawable.ic_activity_icon_request_outgoing
+        "incoming" -> R.drawable.ic_activity_icon_request_incoming
+        "rejected" -> R.drawable.ic_activity_icon_rejected
+        else -> error("incorrect icon provided")
+    }
+    val string = IpfsActivity.summary
+}
