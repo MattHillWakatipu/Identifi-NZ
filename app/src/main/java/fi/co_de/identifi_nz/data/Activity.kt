4bba02158855d8dab2b9228742d0332a8019efa8
matt <matthillwakatipu@gmail.com>
2022-05-19 12:43:44 +1200

    Update colour scheme for accessibility

--- app/src/main/java/fi/co_de/identifi_nz/data/Activity.kt
+++ app/src/main/java/fi/co_de/identifi_nz/data/Activity.kt
@@ -7,7 +7,7 @@ import fi.co_de.identifi_nz.network.json.IpfsActivity
 class Activity(ipfsActivity: IpfsActivity) {
     @DrawableRes
     val imageResourceId = when (ipfsActivity.icon) {
-        "verified" -> R.drawable.ic_identity_fragment_status_verified
+        "verified" -> R.drawable.ic_activity_icon_verified
         "outgoing" -> R.drawable.ic_activity_icon_request_outgoing
         "incoming" -> R.drawable.ic_activity_icon_request_incoming
         "rejected" -> R.drawable.ic_activity_icon_rejected
